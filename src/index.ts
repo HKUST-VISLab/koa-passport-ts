bf9b1f9e4d6a2b3ccd7b2aa0b374390599c02079
chenzhutian <zhutian.chen.code@outlook.com>
2017-11-14 14:07:48 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { AuthenticationError } from './authenticationerror';
+export { Authenticator as Passport } from './authenticator';
+export { HKMOOCStrategy } from './strategies';
---
+++ src/strategies/index.ts
@@ -0,0 +1,11 @@
+export {
+    BaseStrategy,
+    ActionType,
+    BaseAction,
+    PassAction,
+    FailAction,
+    RedirectAction,
+    SuccessAction,
+} from './base';
+export { SessionStrategy } from './session';
+export { HKMOOCStrategy } from './HKMOOCStrategy';
